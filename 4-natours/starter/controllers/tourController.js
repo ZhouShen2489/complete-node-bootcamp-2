28c85e499e64745e4bfe31e196e40b51562e6d01
zhou shen <zhoushen@Mac.attlocal.net>
2025-02-24 14:15:36 -0800

    Natours-Ch8-MVC structure

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,26 +1,22 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+const Tour = require('./../controllers/tourController');
 
 // 2) Route Handler
 
-exports.checkID = (req, res, next, val) => {
-  if (val > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID!',
-    });
-  }
-  next();
-};
+// exports.checkID = (req, res, next, val) => {
+//   if (val > tours.length) {
+//     return res.status(404).json({
+//       status: 'fail',
+//       message: 'Invalid ID!',
+//     });
+//   }
+//   next();
+// };
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
       status: 'fail',
-      message: 'Missing name or price',
+      message: 'Missing name or price'
     });
   }
   next();
@@ -30,61 +26,47 @@ exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
+    requestedAt: req.requestTime
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
 exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
+  // const id = req.params.id * 1;
+  // const tour = tours.find(el => el.id === id);
 
   res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
+    status: 'success'
+    // data: {
+    //   tour
+    // }
   });
 };
 
 exports.createTour = (req, res) => {
-  // console.log(req.body);
-
-  const newID = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newID }, req.body);
-  // merge two object together
-  // req.body use the middleware here to access the req.body
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          newTour,
-        },
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'success'
+    // data: {
+    //   newTour
+    // }
+  });
 };
 
 exports.updateTour = (req, res) => {
   res.status(200).json({
     status: 'success',
     data: {
-      tour: '<Updated tour here...>',
-    },
+      tour: '<Updated tour here...>'
+    }
   });
 };
 
 exports.deleteTour = (req, res) => {
   res.status(204).json({
     status: 'success',
-    data: null,
+    data: null
   });
 };
