b492735e6d5e9b3f5f397cce7782c23e9dac068e
zhou shen <zhoushen@Mac.attlocal.net>
2025-03-21 17:09:36 -0700

    Natours-Ch10-route-protect-finished

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -17,7 +17,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm
+    passwordConfirm: req.body.passwordConfirm,
+    passwordChangedAt: req.body.passwordChangedAt
   });
 
   const token = signToken(newUser._id);
@@ -61,7 +62,6 @@ exports.protect = catchAsync(async (req, res, next) => {
   ) {
     token = req.headers.authorization.split(' ')[1];
   }
-  console.log(token);
 
   if (!token) {
     return next(
@@ -71,11 +71,21 @@ exports.protect = catchAsync(async (req, res, next) => {
   // 2) Verification token
 
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  console.log(decoded);
 
   // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser)
+    return next(
+      new AppError('The user belonging to this token is no longer exist!', 401)
+    );
 
   // 4) Check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat))
+    return next(
+      new AppError('User recently changed password! please log in again', 401)
+    );
 
+  // GRANT ACCESS to protected route
+  req.user = currentUser;
   next();
 });
--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -31,6 +31,9 @@ const userSchema = new mongoose.Schema({
       },
       message: 'The passwords are not the same!'
     }
+  },
+  passwordChangedAt: {
+    type: Date
   }
 });
 
@@ -51,6 +54,18 @@ userSchema.methods.correctPassword = async function(
   userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    return changedTimestamp > JWTTimestamp;
+  }
+  // false measn NOT changed
+  return false;
 };
 
 const User = mongoose.model('User', userSchema);
