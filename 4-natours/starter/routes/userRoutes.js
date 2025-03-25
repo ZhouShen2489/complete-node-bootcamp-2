077f32e8dfef822f1d0bf0308db2c42e08bda4fd
zhou shen <zhoushen@Mac.attlocal.net>
2025-03-24 17:11:29 -0700

    Natours-Ch10-SendResetPasswordEmail

--- 4-natours/starter/routes/userRoutes.js
+++ 4-natours/starter/routes/userRoutes.js
@@ -7,6 +7,9 @@ const router = express.Router();
 router.route('/signup').post(authController.signup);
 router.route('/login').post(authController.login);
 
+router.route('/forgetPassword').post(authController.forgetPassword);
+// router.route('/resetPassword/:token').patch(authController.resetPassword);
+
 router
   .route('/')
   .get(userController.getAllUsers)
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 process.on('uncaughtException', err => {
   console.log('UNCAUGHT EXCEPTION! Shutting Down...');
-  console.log(err.name, err.message);
+  console.log(err);
   process.exit(1);
 });
 
