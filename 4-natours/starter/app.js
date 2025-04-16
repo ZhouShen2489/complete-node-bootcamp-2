43880062204b0d589cf91cfacfbf9f94f58dd88a
ZhouShen2489 <zs2489@columbia.edu>
2025-04-15 22:55:16 -0700

    Natours-Ch12-Pug-base-header&footer

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,10 +15,15 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 // 1) Global Middleware
 
 // Set security HTTP headers
 app.use(helmet());
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public'))); //access static files in folder
 
 // Log development
 if (process.env.NODE_ENV === 'development') {
@@ -56,9 +62,6 @@ app.use(
   })
 );
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`)); //access static files in folder
-
 // Test the middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -66,7 +69,12 @@ app.use((req, res, next) => {
   next();
 });
 // 3) Routes
-
+app.use('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'zhou shen'
+  });
+});
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
