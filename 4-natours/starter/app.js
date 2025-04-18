a771473cfcece4b1c60386cc16d466b644e89e28
zhou shen <zhoushen@Mac.attlocal.net>
2025-04-18 16:51:04 -0700

    Natours-Ch12-pug extend

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -70,13 +70,27 @@ app.use((req, res, next) => {
   // console.log(req.headers);
   next();
 });
+
 // 3) Routes
+app.use('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours'
+  });
+});
+
+app.use('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker'
+  });
+});
+
 app.use('/', (req, res) => {
   res.status(200).render('base', {
     tour: 'The Forest Hiker',
     user: 'zhou shen'
   });
 });
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
