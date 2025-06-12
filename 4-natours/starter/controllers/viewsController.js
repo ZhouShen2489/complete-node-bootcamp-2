6a9582543a2fb2dd778e1f2f1519188fe2eb9f40
ZhouShen2489 <zs2489@columbia.edu>
2025-06-11 18:01:27 -0700

    Matours-Ch12-Build Overview Pug

--- 4-natours/starter/controllers/viewsController.js
+++ 4-natours/starter/controllers/viewsController.js
@@ -1,8 +1,18 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1.Get all the tours
+  const tours = await Tour.find();
+
+  // 2. Build template
+
+  // 3. Render all cards
   res.status(200).render('overview', {
-    title: 'All Tours'
+    title: 'All Tours',
+    tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
