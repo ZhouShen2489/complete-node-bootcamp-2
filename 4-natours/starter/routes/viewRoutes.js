3ad8bd2a8e5c75009aed2d812e564047b5a27c34
ZhouShen2489 <zs2489@columbia.edu>
2025-07-18 00:55:23 -0700

    Natours-ch12-logging in users-part1

--- 4-natours/starter/routes/viewRoutes.js
+++ 4-natours/starter/routes/viewRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const viewsControlloer = require('../controllers/viewsController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
 router.get('/', viewsControlloer.getOverview);
 
-router.get('/tour/:slug', viewsControlloer.getTour);
+router.get('/tour/:slug', authController.protect, viewsControlloer.getTour);
+
+router.get('/login', viewsControlloer.getLoginForm);
 
 module.exports = router;
